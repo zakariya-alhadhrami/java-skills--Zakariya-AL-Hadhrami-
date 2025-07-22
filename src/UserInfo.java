63f7e8980496c984d1c465e03e6fd15b5a889a6c
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-22 14:18:31 +0400

    creating the private variables for encabsulation

--- src/UserInfo.java
+++ src/UserInfo.java
@@ -1,3 +1,20 @@
 public class UserInfo {
 
+    private String name;
+    private int age;
+    private String email;
+    private boolean isActive;
+
+
+    public UserInfo(String name,int age, String email,boolean isActive ){
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.isActive = isActive;
+    }
+    
+
+
+
+
 }
