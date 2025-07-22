bccf4da28afb48f942d08d27ba268700d838e9bc
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-22 22:48:56 +0400

    did the static vs not static part to practice and then will do the testing.

--- src/VisitorCounter.java
+++ src/VisitorCounter.java
@@ -1,2 +1,33 @@
 public class VisitorCounter {
+
+    private static int totalVisitors;
+
+    private int sessionVisits;
+    private String visitorName;
+
+    public VisitorCounter(String name) {
+        this.visitorName = name;
+        this.sessionVisits =0;
+        totalVisitors++;
+
+    }
+
+    public void recordVisits(){
+        sessionVisits++;
+        System.out.println(visitorName+" Visited. Session Visits: "+sessionVisits);
+    }
+
+    public static void displayTotalVisitors(){
+
+        System.out.println("Total Visitors Today: "+totalVisitors);
+
+    }
+
+    public static int getTotalVisitors() {
+        return totalVisitors;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
