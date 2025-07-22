50df4284ac461731f08f2961f8efa7887b591f6a
Zakariya AlHadhrami <zakarya601@gmail.com>
2025-07-22 21:09:13 +0400

    adding the method for checking number of words.

--- src/Textprocessor.java
+++ src/Textprocessor.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
 public class Textprocessor {
 
-    
+
+    public static int countWords(String sentence) {
+        Scanner input = new Scanner(System.in);
+
+
+        if(sentence.isEmpty()==true){
+            System.out.println("sentence is Empty");
+
+        }
+        else {
+
+            String[] words = sentence.split(" ");
+            int count = words.length;
+            return count;
+
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        
+
+
+
+    }
 }
